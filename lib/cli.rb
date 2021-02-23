01373be9e6c46ef3f3f2392f2d59179cdf0052ce
Jessica Joseph <jessicaajosephh@gmail.com>
2021-02-23 11:31:34 -0500

    added a call method to the cli.rb and then created a greeting method that the call method will invoke

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,15 @@
 class CLI
 
+    def call
+        greeting
+        menu
+    end
+
+    def greeting
+        puts "Hello, welcome to the Trivia Challenge!"
+        puts "To begin the challenge, enter 'begin'"
+        puts "If you're not up for the challenge, enter 'exit'"
+    end
+
 
 end
